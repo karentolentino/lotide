0368808b6363f72c923305a9a0a5539fd97e638d
Karen Tolentino <k.tolentino@live.ca>
2021-09-16 01:03:18 +0000

    findKeyByValue.js completed

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  let objectKeys = Object.keys(object);
+  let index = 0;
+  for (let element in object) {
+    if (object[element] === value) {
+      return objectKeys[index];
+    }
+    index++;
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
